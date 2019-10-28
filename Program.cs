f3b3535f9ea1e8adf4e633217ab4945b2fe80a3e
dek-haji <deeeqhaji@gmail.com>
2019-10-28 16:51:54 -0500

    added initial website run using selenium

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.IO;
+using System.Reflection;
+
+namespace web_scraping_demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IWebDriver driver = new ChromeDriver(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location));
+            driver.Navigate().GoToUrl("https://nashville.craigslist.org/");
+            driver.FindElement(By.ClassName("aos")).Click();
+            driver.FindElement(By.ClassName("srchType")).Click();
+
+        }
+    }
+}
